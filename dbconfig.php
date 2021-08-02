de1678f0b64735135db27f35706dc691f7fca3e4
Nixnet <54565029+Vald0@users.noreply.github.com>
2021-08-02 17:17:41 -0500

    Update dbconfig.php
    tabien

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-$dbHost     = "localhost";
+$dbHost     = "tabien";
 $dbUsername = "xpresion_lahuerta";
 $dbPassword = "!Sop4s!";
 $dbName     = "xpresion_lahuerta_db";
