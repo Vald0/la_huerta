d6bc107cd7d7a6aea4e010ada733fa5e8c788c2b
Nixnet <54565029+Vald0@users.noreply.github.com>
2021-08-02 00:45:08 -0500

    omg

--- dbconfig.php
+++ dbconfig.php
@@ -4,7 +4,6 @@ $dbHost     = "189.179.131.216:3306";
 $dbUsername = "xpresion_lahuerta";
 $dbPassword = "!Sop4s!";
 $dbName     = "xpresion_lahuerta_db";
-//OMG
 // Create database connection
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
